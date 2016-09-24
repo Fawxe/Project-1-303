343d443ff73e62c2ae6385cdd92d2b802ae50055
Jacob Quick <jrq5y3@mail.umkc.edu>
2016-09-24 16:11:22 -0500

    Update Term.h

--- Term.h
+++ Term.h
@@ -1,6 +1,7 @@
 #pragma once
 
 using namespace std;
+#include <string>;
 
 
 class Term {
@@ -11,6 +12,17 @@ private:
 
 
 public:
+	int get_exponent() { return exponent; }
+	int get_coefficient() { return coefficient; }
+	void set_exponent(int new_expo) { exponent = new_expo; }
+	void set_coefficient(int new_coeff) { coefficient = new_coeff; }
 
+	friend bool operator<(const Term& left, const Term& right)
+	{
+		return (left.exponent < right.exponent);
+	}
 
 };
+
+
+
