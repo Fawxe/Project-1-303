dbac93a513d7fdaf9f69188cdb97493b1ccda95b
Jacob Quick <jrq5y3@mail.umkc.edu>
2016-09-24 22:55:08 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -2,17 +2,30 @@
 
 using namespace std;
 #include <iostream>;
-
-class Menu {
-
-private:
+#include <string>;
 
 
 
-public:
+	string Get_Choice(bool complete)//complete shows if there is something to display
+	{
+		{
+			cout << "________Menu________" << endl;
+			cout << "Please select from the options below.(use the number representing the choice)" << endl << endl;
+			cout << "1) Enter First Polynomial" << endl;
+			cout << "2) Add Polynomial" << endl;
+			cout << "3) Display Polynomial Again" << endl;
+			cout << "4) Exit Program" << endl;
 
-	void Display_list();
-	void Get_Input();
+		}
+		string choice;
+		cin >> choice;
+		return choice;
+	}
 
-
-};
+	string Get_Input() 
+	{
+		cout << "Please enter Your Polynomial in the following form.   3X^3–1X^2+1" << endl;
+		string input;
+		cin >> input;
+		return input;
+	}
