f02a9fa3e2afa4600a43dcc43909d6919c89a4da
Jacob Quick <jrq5y3@mail.umkc.edu>
2016-09-25 21:47:22 -0500

    Update Term.h

--- Term.h
+++ Term.h
@@ -7,22 +7,36 @@ using namespace std;
 class Term {
 
 private:
-	int exponent;
-	int coefficient;
+	int exponent = 0;
+	int coefficient = 0;
 
 
 public:
-	int get_exponent() { return exponent; }
-	int get_coefficient() { return coefficient; }
-	void set_exponent(int new_expo) { exponent = new_expo; }
-	void set_coefficient(int new_coeff) { coefficient = new_coeff; }
+	int get_exponent() { return exponent; }  //simple getter
+	int get_coefficient() { return coefficient; } //simple getter
+	void set_exponent(int new_expo) { exponent = new_expo; } //simple setter
+	void set_coefficient(int new_coeff) { coefficient = new_coeff; } //simple setter
 
-	friend bool operator<(const Term& left, const Term& right)
+
+
+	friend bool operator<(Term left, Term right)
 	{
 		return (left.exponent < right.exponent);
 	}
 
+	friend bool operator==(const Term& left, const Term& right) //checking only exponent is necessary 
+	{
+		return (left.exponent == right.exponent);
+	}
+
+	friend Term operator+(Term& left, Term& right) //allows addition of Terms
+	{
+		left.coefficient = right.coefficient + left.coefficient; //issues with additioin caused more specific declaration
+		return left;
+	}
+
 };
+
 
 
 
