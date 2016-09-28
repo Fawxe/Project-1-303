45924d00bc8da3b578797d64ce5afc44ff37a050
Jacob Quick <jrq5y3@mail.umkc.edu>
2016-09-27 22:26:54 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -6,48 +6,48 @@ using namespace std;
 #include "list.h";
 #include "Term.h";
 
-bool Validate_input(string validCharacters, string input)
+bool Validate_input(string validCharacters, string input) //insures that only valid input is recieved
 {
-	for (int i = 0; i < input.size(); i++)
+	for (int i = 0; i < input.size(); i++) //iterates through string input
 	{
-		if (validCharacters.find(input[i]) == -1)
+		if (validCharacters.find(input[i]) == -1) //look for character 
 		{
 			cout << endl << input[i];
-			cout << " is not a valid input character." << endl << endl;
+			cout << " is not a valid input character." << endl << endl;  //inform user if it isn't there
 			return false;
 		}
 	}
 	return true;
 }
 
-string Get_Choice(string firstPolynomial, string secondPolynomial)
+string Get_Choice(string firstPolynomial, string secondPolynomial) // first menu allows user to choose action
 {
 	bool validInput = false;
 	string choice;
-	string validInputString = "012";
+	string validInputString = "012";  
 	while (!validInput)
 	{
 		cout << endl;
-		cout << "________Menu________" << endl;
+		cout << "________Menu________" << endl;  //initial choice to allow input of the 2 polynomials
 		cout << "Please select from the options below.(use the number representing the choice)" << endl << endl;
 		cout << "0) Exit Program" << endl;
 		cout << "1) Enter the 1st Polynomial ";
 		if (firstPolynomial.size() > 0)
-			cout << ": (" << firstPolynomial << ")" << endl;
+			cout << ": (" << firstPolynomial << ")" << endl;  //collects first polynomial
 		else
 			cout << endl;
 
 		cout << "2) Enter the 2nd Polynomial ";
 		if (secondPolynomial.size() > 0)
-			cout << ": (" << secondPolynomial << ")" << endl;
+			cout << ": (" << secondPolynomial << ")" << endl; //collects second
 		else
 			cout << endl;
-		if (firstPolynomial.size() > 0 && secondPolynomial.size() > 0) {
+		if (firstPolynomial.size() > 0 && secondPolynomial.size() > 0) {  //opens up more options if there are polynomials to use
 
 			cout << "3) Add the two Polynomials. " << endl;
 			cout << "4) Add the two Polynomials and replace the first with the result." << endl;
 			cout << "[0-4]:";
-			validInputString = "01234";
+			validInputString = "01234";  //increase valid characters
 		}
 		else {
 			cout << "[0-2]:";
@@ -58,7 +58,7 @@ string Get_Choice(string firstPolynomial, string secondPolynomial)
 	return choice;
 }
 
-string Get_Input()
+string Get_Input() //prompts for and recieves the polynomial
 {
 	bool validInput = false;
 	string input;
@@ -72,36 +72,37 @@ string Get_Input()
 	return input;
 }
 
-string Display_polynomial(list<Term> polynomial, string firstPolynomial, string secondPolynomial)
+string Display_polynomial(list<Term> polynomial, string firstPolynomial, string secondPolynomial) //outputs the polynomial in the proper format
 {
 	string outputPolynomial = "";
-	list<Term>::iterator itr = polynomial.begin();
+	list<Term>::iterator itr = polynomial.begin();  //set up iterator for the list
 	Term first = *itr;
 	if (first.get_coefficient() != 0)
-		outputPolynomial = to_string(first.get_coefficient());
-	if (first.get_exponent() > 0)
+		outputPolynomial = to_string(first.get_coefficient());  // this first part is to allow no + signs in the first term
+	if (first.get_exponent() != 0)
 	{
 		outputPolynomial = outputPolynomial + "x";
-		if (first.get_exponent() > 1)
-			outputPolynomial = outputPolynomial + "^" + to_string(first.get_exponent());
+		outputPolynomial = outputPolynomial + "^" + to_string(first.get_exponent());
 	}
-	itr++;
-	for (itr; itr != polynomial.end(); itr++)
+	itr++;  //move iterator to the second term to continue the display
+	for (itr; itr != polynomial.end(); itr++)  //iterate through terms
 	{
 		Term Node = *itr;
 		int coefficient = Node.get_coefficient();
 		int exponent = Node.get_exponent();
 		if (coefficient > 0)			//this if statement adds an operator to positive numbers for output
 			outputPolynomial = outputPolynomial + "+" + to_string((*itr).get_coefficient());
+		else if (coefficient < 0 )
+			outputPolynomial = outputPolynomial + to_string((*itr).get_coefficient());  //changes the sign for negative numbers
 		else if (coefficient == 0)
 			continue;
 		else
 			outputPolynomial = outputPolynomial + to_string((*itr).get_coefficient());
 
-		if (exponent > 0)
+		if (exponent != 0)
 		{
 			outputPolynomial = outputPolynomial + "x";
-			if (exponent > 1)
+			if (exponent > 1||exponent<0)
 			{
 				outputPolynomial = outputPolynomial + "^" + to_string((*itr).get_exponent());
 			}
@@ -109,7 +110,7 @@ string Display_polynomial(list<Term> polynomial, string firstPolynomial, string 
 	}
 	cout << endl;
 	cout << "  " << firstPolynomial << endl;
-	cout << "+ " << secondPolynomial << endl;
+	cout << "+ " << secondPolynomial << endl;  //outputs addition and result
 	cout << "= " << outputPolynomial << endl;
 	return outputPolynomial;
 }
